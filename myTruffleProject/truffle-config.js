e494049393af05e1a03cd230ad72b4bdf2d158bb
Prasad Pathak <prasaddpathak@gmail.com>
2021-09-12 20:44:00 +0530

    fixed untracked files

--- myTruffleProject/truffle-config.js
+++ myTruffleProject/truffle-config.js
@@ -41,11 +41,17 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 8545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+    },
+    
+    geth: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 30303,            // Standard Ethereum port (default: none)
+     network_id: "2021",       // Any network (default: none)
+    }
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
